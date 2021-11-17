c1f626ef52b4c4b4e1ad570114ddd92f9555f834
rvrs <rev3rsestudio@gmail.com>
2021-11-17 15:15:37 +0100

    init commit

---
+++ src/main/java/com/rvrs886/employees/model/Employee.java
@@ -0,0 +1,33 @@
+package com.rvrs886.employees.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email_id", nullable = false)
+    private String emailId;
+
+}
