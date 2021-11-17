8a18f47f6e83399eff2f7acb6700b9a1416b5437
rvrs <rev3rsestudio@gmail.com>
2021-11-17 15:17:58 +0100

    Controller added

---
+++ src/main/java/com/rvrs886/employees/controller/EmployeeController.java
@@ -0,0 +1,7 @@
+package com.rvrs886.employees.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class EmployeeController {
+}
--- src/main/java/com/rvrs886/employees/repository/EmployeeRepository.java
+++ src/main/java/com/rvrs886/employees/repository/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.rvrs886.employees.repository;
 
 import com.rvrs886.employees.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
