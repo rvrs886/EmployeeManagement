1cc4cb931a5f185d4557ce422dd11139586eec92
rvrs <rev3rsestudio@gmail.com>
2021-11-17 15:57:18 +0100

    configuration for postrgresql database, controller for Employee

--- src/main/java/com/rvrs886/employees/controller/EmployeeController.java
+++ src/main/java/com/rvrs886/employees/controller/EmployeeController.java
@@ -1,7 +1,72 @@
 package com.rvrs886.employees.controller;
 
+import com.rvrs886.employees.model.Employee;
+import com.rvrs886.employees.repository.EmployeeRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class EmployeeController {
+
+    EmployeeRepository employeeRepository;
+
+    EmployeeController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @GetMapping("/employees")
+    public ResponseEntity<List<Employee>> getAllEmployees(){
+        return ResponseEntity.ok(employeeRepository.findAll());
+    }
+
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id){
+        Optional<Employee> employee = employeeRepository.findById(id);
+
+        if (employee.isPresent()){
+            return new ResponseEntity<>(employee.get(), HttpStatus.FOUND);
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/employees")
+    public ResponseEntity<Employee> saveEmployee(@RequestBody Employee newEmployee){
+        Employee employee = employeeRepository.save(newEmployee);
+        return ResponseEntity.ok(employee);
+    }
+
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee updatedEmployee){
+        Optional<Employee> employee = employeeRepository.findById(id);
+
+        if (employee.isPresent()){
+            employee.get().setFirstName(updatedEmployee.getFirstName());
+            employee.get().setLastName(updatedEmployee.getLastName());
+            employee.get().setEmailId(updatedEmployee.getEmailId());
+            Employee newEmployee = employeeRepository.save(employee.get());
+            return new ResponseEntity<>(newEmployee, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(updatedEmployee, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public ResponseEntity<Long> deleteEmployee(@PathVariable Long id){
+        Optional<Employee> employee = employeeRepository.findById(id);
+
+        if (employee.isPresent()){
+            employeeRepository.delete(employee.get());
+            return new ResponseEntity<>(id, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(id, HttpStatus.NOT_FOUND);
+        }
+    }
+
 }
