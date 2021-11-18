4d2cb2240deec7a4eaa9403ebbba4578040a9dbd
rvrs <rev3rsestudio@gmail.com>
2021-11-18 19:45:42 +0100

    - created ui showing list of employees in table view - added creating new employee by form

--- src/main/java/com/rvrs886/employees/EmployeesApplication.java
+++ src/main/java/com/rvrs886/employees/EmployeesApplication.java
@@ -12,39 +12,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.batch.BatchProperties;
 
 @SpringBootApplication
-public class EmployeesApplication implements CommandLineRunner {
+public class EmployeesApplication implements CommandLineRunner{
 
     public static void main(String[] args) {
         SpringApplication.run(EmployeesApplication.class, args);
     }
 
-    EmployeeRepository employeeRepository;
-    DepartmentRepository departmentRepository;
-    JobPositionRepository jobPositionRepository;
-
-    EmployeesApplication(EmployeeRepository employeeRepository, DepartmentRepository departmentRepository, JobPositionRepository jobPositionRepository) {
-        this.employeeRepository = employeeRepository;
-        this.departmentRepository = departmentRepository;
-        this.jobPositionRepository = jobPositionRepository;
-    }
-
     @Override
     public void run(String... args) throws Exception {
-//        Department department1 = new Department("IT");
-//        departmentRepository.save(department1);
-//        Department department2 = new Department("HR");
-//        departmentRepository.save(department2);
-//
-//        JobPosition jobPosition1 = new JobPosition("Junior Java Developer");
-//        jobPositionRepository.save(jobPosition1);
-//        JobPosition jobPosition2 = new JobPosition("Junior HR Specialist");
-//        jobPositionRepository.save(jobPosition2);
-//
-//        Employee employee1 = new Employee("Kacper", "Lesniak", "rev3rsestudio@gmail.com", department1, jobPosition1);
-//        employeeRepository.save(employee1);
-//        Employee employee2 = new Employee("Gaja", "Dabal", "gajadabal123@wp.pl",department2, jobPosition2);
-//        employeeRepository.save(employee2);
-//        Employee employee3 = new Employee("Adrian", "Pielech", "adrian@gmail.com", department1, jobPosition2);
-//        employeeRepository.save(employee3);
+
     }
 }
--- src/main/java/com/rvrs886/employees/controller/EmployeeController.java
+++ src/main/java/com/rvrs886/employees/controller/EmployeeController.java
@@ -1,26 +1,28 @@
 package com.rvrs886.employees.controller;
 
+import com.rvrs886.employees.model.Department;
 import com.rvrs886.employees.model.Employee;
+import com.rvrs886.employees.model.JobPosition;
+import com.rvrs886.employees.repository.DepartmentRepository;
 import com.rvrs886.employees.repository.EmployeeRepository;
 import com.rvrs886.employees.repository.JobPositionRepository;
-import org.dom4j.rule.Mode;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.util.List;
-import java.util.Optional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class EmployeeController {
 
     EmployeeRepository employeeRepository;
+    DepartmentRepository departmentRepository;
+    JobPositionRepository jobPositionRepository;
 
-    EmployeeController(EmployeeRepository employeeRepository) {
+    EmployeeController(EmployeeRepository employeeRepository, DepartmentRepository departmentRepository, JobPositionRepository jobPositionRepository) {
         this.employeeRepository = employeeRepository;
+        this.departmentRepository = departmentRepository;
+        this.jobPositionRepository = jobPositionRepository;
     }
 
     @GetMapping("/employees")
@@ -29,5 +31,26 @@ public class EmployeeController {
         return "employees";
     }
 
+    @GetMapping("/employees/create")
+    public String createEmployeeForm(Model model){
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        model.addAttribute("departments", departmentRepository.findAll());
+        model.addAttribute("jobPositions", jobPositionRepository.findAll());
+        return "create_employee";
+    }
+
+    @PostMapping("employees")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee){
+        Department department = departmentRepository.getById(employee.getDepartment().getId());
+        JobPosition jobPosition = jobPositionRepository.getById(employee.getJobPosition().getId());
+
+        employee.setDepartment(department);
+        employee.setJobPosition(jobPosition);
+
+        employeeRepository.save(employee);
+
+        return "redirect:/employees";
+    }
 
 }
